3be4755fd772670f2f33653439f77fabf604b523
sahilpatel-2610 <kakadiyasahil102@gmail.com>
2025-07-14 10:23:23 +0530

    interview question on counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,7 +10,9 @@ function App() {
   const addValue = () => {
     // counter = counter + 1
     setCounter(prevCounter => prevCounter + 1)
-    
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)  
 }
 
   const removeValue = () => {
